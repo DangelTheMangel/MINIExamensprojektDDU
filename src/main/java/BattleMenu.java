365542a04c5fd0d7461eb08478f1f269fc023733
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-11-03 14:03:34 +0100

    Har lavet en smuk arraylist hehe. Mit liv er complet. Christians diller knudder åbenbart lidt mærkeligt hvis du spørger mig men hver man for sig selv. Min diller bliver bare sten bagt i en god gammeldags pizza ovn. MMmMMMMM pepperoni pizaa. mozarrela.
    Casper BOOMER
    
    vnelig hilsen Gokkebro

--- src/main/java/BattleMenu.java
+++ src/main/java/BattleMenu.java
@@ -1,5 +1,18 @@
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+
 public class BattleMenu {
     PApplet p;
+    ArrayList<QaustionsData> spagersmal = new ArrayList<QaustionsData>();
+    BattleMenu(){}
+
+        void drawBattleMenu(){
+
+            
+
+        }
+
+
+
 }
