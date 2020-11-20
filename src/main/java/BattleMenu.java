f843f34bb02b41e6bd4a83b8c4ba030a4b05ba0d
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-11-20 14:55:37 +0100

    h

--- src/main/java/BattleMenu.java
+++ src/main/java/BattleMenu.java
@@ -11,11 +11,13 @@ public class BattleMenu {
     float eLife=100, pLife=100;
     int firstQ = 0,middleQ= 1,lastQ = 2 , chosen;
     int oldsum;
+    boolean logOutKnap = false;
     PApplet p;
     ArrayList<QaustionsData> spagersmal = new ArrayList<QaustionsData>();
-    AlmindeligKnap QaustionOne, QaustionTwo, QaustionThree;
+    AlmindeligKnap QaustionOne, QaustionTwo, QaustionThree, logOut;
     Table qaustions;
     HealthBar EHealthBar, PHealthBar;
+    LoginSide ls;
     Qaustions qs;
     DataHolder dh;
     BattleMenu(PApplet p,Table qaustions){
@@ -25,6 +27,7 @@ public class BattleMenu {
         QaustionOne= new AlmindeligKnap(p,350,500,200,220, "1");
         QaustionTwo = new AlmindeligKnap(p,550,500,200,220, "2");
         QaustionThree = new AlmindeligKnap(p,750,500,200,220, "3");
+        logOut = new AlmindeligKnap(p,200,500,700,200, "Log out");
         qs = new Qaustions(p,qaustions);
         makeQaustions();
         changeBtnName(QaustionOne, spagersmal.get(firstQ).qaustion.getString(0,0));
@@ -103,7 +106,9 @@ public class BattleMenu {
             QaustionTwo.registrerKlik(mouseX, mouseY);
             QaustionThree.registrerKlik(mouseX, mouseY);
             p.println(QaustionOne.erKlikket());
-
+            if(logOutKnap) {
+                logOut.registrerKlik(mouseX, mouseY);
+            }
         } else if(qs.visibal ){
             qs.clicked(mouseX,mouseY);
         }
@@ -151,15 +156,23 @@ public class BattleMenu {
 
         void ifLifeIsZero(HealthBar h){
             if(h.health <= 0){
+                logOutKnap = true;
                 p.clear();                          //vi laver et clear for at lave en sort skærm. Det kunne være fedt med en baggrund.
                 p.fill(255,255,255);                //den her og de 2 under laver teksten som den skal være.
                 p.textAlign(PConstants.CENTER);
+
+                if(logOut.erKlikket()){
+                    visbel = false;
+
+                }
                 p.textSize(64);
                 p.text(dh.elevNavn + " fik " + dh.svaretRigtigt + " point.",p.width/2,p.height/2); //og det her er teksten
                 if(dh.svaretRigtigt==2){            //hvis man får alle rigtige siger den du er sej!
                     p.text(dh.elevNavn+ " vandt!",p.width/2,p.height/2+60);
 
                 }
+
+                logOut.tegnKnap();
             }
         }
     }
