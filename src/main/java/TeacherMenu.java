16fce6f5996e48710720611ca37c32cb6a596fa2
mariuss tokkebro <43348192+mariusstokkebro@users.noreply.github.com>
2020-11-27 17:52:52 +0100

    tekstfelter lavet og knapper men de gør ikke det de skal endnu

--- src/main/java/TeacherMenu.java
+++ src/main/java/TeacherMenu.java
@@ -17,9 +17,7 @@ public class TeacherMenu {
     AlmindeligKnap editorKnap;
     AlmindeligKnap resultKnap;
     AlmindeligKnap addSpergsmalK;
-    AlmindeligKnap addRigtigtSvarK;
-    AlmindeligKnap addSvar2K;
-    AlmindeligKnap addSvar3K;
+
     //forskellige menuer
     boolean menuVisible = true;
     boolean resultVisible = false;
@@ -29,15 +27,14 @@ public class TeacherMenu {
 
         removeSpergsmaal = new TextFlet(p,50,50,200,50,"Fjern Spørgsmål");
         addSpergsmall = new TextFlet(p,50,150,200,50,"Tilføj spørgsmål");
-        addRigtigtSvar = new TextFlet(p,50,250,200,50,"Tilføj rigtigt svar");
-        addSvar2 = new TextFlet(p,50,350,200,50,"tilføj svarmulighed");
-        addSvar3 = new TextFlet(p,50,450,200,50,"tilføj svarmulgihed");
+        addRigtigtSvar = new TextFlet(p,50,250,200,50,"Tilføj korrekt svar");
+        addSvar2 = new TextFlet(p,50,350,200,50,"Tilføj svar mulighed");
+        addSvar3 = new TextFlet(p,50,450,200,50,"Tilføj svar mulighed 2");
+
         editorKnap = new AlmindeligKnap(p, 600, 100, 100, 100, "Editor");
         resultKnap = new AlmindeligKnap(p, 600, 200, 100, 100, "Resultater");
-        addSpergsmalK = new AlmindeligKnap(p,400,150,50,50,"Add");
-        addRigtigtSvarK = new AlmindeligKnap(p,400,250,50,50,"Add");
-        addSvar2K = new AlmindeligKnap(p,400,350,50,50,"Add");
-        addSvar3K = new AlmindeligKnap(p,400,450,50,50,"Add");
+        addSpergsmalK = new AlmindeligKnap(p,50,650,100,50,"Tilføj spørgsmål");
+
         sp = p.loadTable("sp.csv");
     }
     void drawMenu() {
@@ -47,9 +44,6 @@ public class TeacherMenu {
         addSvar2.tegnTextFlet();
         addSvar3.tegnTextFlet();
         addSpergsmalK.tegnKnap();
-        addRigtigtSvarK.tegnKnap();
-        addSvar2K.tegnKnap();
-        addSvar3K.tegnKnap();
 
         p.text("Velkommen" + users.get((int) userID ), 100,100);
         for(int i=1; i<sp.getRowCount(); i++)
@@ -58,36 +52,20 @@ public class TeacherMenu {
             p.saveTable(sp,"sp.csv");
 
         }
-        boolean c = sp.getString(sp.getRowCount()-1, 0).equals("");
 
-        if (addSpergsmalK.klikket &&
-                !sp.getString(sp.getRowCount()-1, 0).equals("")&&
-                !sp.getString(sp.getRowCount()-1, 1).equals("")&&
-                !sp.getString(sp.getRowCount()-1, 2).equals("")&&
-                !sp.getString(sp.getRowCount()-1, 3).equals("")&&
-                !sp.getString(sp.getRowCount()-1, 4).equals("")) {
+
+        if (addSpergsmalK.klikket){
 
             sp.addRow();
-            sp.setString(sp.getRowCount(),0,addSpergsmall.indput);
+            sp.setString(sp.getRowCount()-1,0,addSpergsmall.indput);
+            sp.setString(sp.getRowCount()-1,1,addRigtigtSvar.indput);
+            sp.setString(sp.getRowCount()-1,2,addRigtigtSvar.indput);
+            sp.setString(sp.getRowCount()-1,3,addSvar2.indput);
+            sp.setString(sp.getRowCount()-1,4,addSvar3.indput);
             p.saveTable(sp,"sp.csv");
             addSpergsmalK.registrerRelease();
         }
-        if(addRigtigtSvarK.klikket){
-            sp.setString(sp.getRowCount(),1,addRigtigtSvar.indput);
-            sp.setString(sp.getRowCount(),2,addRigtigtSvar.indput);
-            p.saveTable(sp,"sp.csv");
-            addRigtigtSvarK.registrerRelease();
-        }
-        if(addSvar2K.klikket){
-            sp.setString(sp.getRowCount(),3,addSvar2.indput);
-            p.saveTable(sp,"sp.csv");
-            addSvar2K.registrerRelease();
-        }
-        if(addSvar3K.klikket){
-            sp.setString(sp.getRowCount(),4,addSvar3.indput);
-            p.saveTable(sp,"sp.csv");
-            addSvar3K.registrerRelease();
-        }
+
 
 
         if(menuVisible) {
@@ -122,9 +100,7 @@ public class TeacherMenu {
     }
     void clik(float mx, float my) {
         addSpergsmalK.registrerKlik(p.mouseX,p.mouseY);
-        addRigtigtSvarK.registrerKlik(p.mouseX,p.mouseY);
-        addSvar2K.registrerKlik(p.mouseX, p.mouseY);
-        addSvar3K.registrerKlik(p.mouseX,p.mouseY);
+
 
         removeSpergsmaal.KlikTjek(mx, my);
         addSpergsmall.KlikTjek(mx, my);
