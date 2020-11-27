7386b72add768dda03bd15a89e028a9ddc152084
Christian Randleff <cra@minuba.dk>
2020-11-27 21:25:03 +0100

    tilføjet noget

--- src/main/java/TeacherMenu.java
+++ src/main/java/TeacherMenu.java
@@ -11,12 +11,13 @@ public class TeacherMenu {
     TextFlet addSpergsmall;
     TextFlet addRigtigtSvar;
     TextFlet addSvar2;
-    TextFlet addSvar3;
+    TextFlet addSvar3 , hold;
     Table sp;
     boolean visibal = false; //christian visibel
-    AlmindeligKnap editorKnap;
+    AlmindeligKnap editorKnap , backToMenu, nyElev;
     AlmindeligKnap resultKnap;
-    AlmindeligKnap addSpergsmalK;
+    AlmindeligKnap addSpergsmalK, elevEn,elevTo,elevTre;
+
 
     //forskellige menuer
     boolean menuVisible = true;
@@ -24,34 +25,58 @@ public class TeacherMenu {
     boolean editorVisible = false;
     TeacherMenu(PApplet p) {
         this.p = p;
-
+        elevEn = new AlmindeligKnap(p,50,220,p.width/2-100,50,"1");
+        elevTo = new AlmindeligKnap(p,50,320,p.width/2-100,50,"2");
+        elevTre = new AlmindeligKnap(p,50,420,p.width/2-100,50,"3");
+        nyElev = new AlmindeligKnap(p,50,520,p.width/2-100,50,"Ny elev");
+        hold = new TextFlet(p,50,120,p.width/2-100,50,"Hold");
         removeSpergsmaal = new TextFlet(p,50,50,200,50,"Fjern Spørgsmål");
         addSpergsmall = new TextFlet(p,50,150,200,50,"Tilføj spørgsmål");
         addRigtigtSvar = new TextFlet(p,50,250,200,50,"Tilføj korrekt svar");
         addSvar2 = new TextFlet(p,50,350,200,50,"Tilføj svar mulighed");
         addSvar3 = new TextFlet(p,50,450,200,50,"Tilføj svar mulighed 2");
-
-        editorKnap = new AlmindeligKnap(p, 600, 100, 100, 100, "Editor");
-        resultKnap = new AlmindeligKnap(p, 600, 200, 100, 100, "Resultater");
+        backToMenu = new AlmindeligKnap(p,p.width/2 - 100,600,200,50,"back to menu");
+        editorKnap = new AlmindeligKnap(p, 50, 100, p.width-100, 100, "Editor");
+        resultKnap = new AlmindeligKnap(p, 50, 220, p.width-100, 100, "Resultater");
         addSpergsmalK = new AlmindeligKnap(p,50,650,100,50,"Tilføj spørgsmål");
 
         sp = p.loadTable("sp.csv");
     }
     void drawMenu() {
-        removeSpergsmaal.tegnTextFlet();
-        addSpergsmall.tegnTextFlet();
-        addRigtigtSvar.tegnTextFlet();
-        addSvar2.tegnTextFlet();
-        addSvar3.tegnTextFlet();
-        addSpergsmalK.tegnKnap();
+        if(resultVisible){
+            backToMenu.tegnKnap();
+            drawReMenu();
+        }
+        if(backToMenu.klikket){
+            menuVisible = true;
+            resultVisible = false;
+            editorVisible = false;
+            backToMenu.registrerRelease();
+        }
+        if(editorKnap.klikket){
+            menuVisible = false;
+            resultVisible = false;
+            editorVisible = true;
+            editorKnap.registrerRelease();
+        }
 
-        p.text("Velkommen" + users.get((int) userID ), 100,100);
+        if(resultKnap.klikket){
+            menuVisible = false;
+            resultVisible = true;
+            editorVisible = false;
+            resultKnap.registrerRelease();
+        }
+
+
+
         for(int i=1; i<sp.getRowCount(); i++)
         if(removeSpergsmaal.indput.equals(sp.getString(i,0))){
             sp.removeRow(i);
             p.saveTable(sp,"sp.csv");
 
         }
+
+
 
 
         if (addSpergsmalK.klikket){
@@ -75,6 +100,14 @@ public class TeacherMenu {
             resultKnap.registrerKlik(200, 100);
         }
 
+        if(editorVisible){
+            backToMenu.tegnKnap();
+            drawSPMenu();
+        }
+
+
+
+
         if (resultKnap.klikket){
             visibal = false;
             resultVisible = true;
@@ -85,28 +118,63 @@ public class TeacherMenu {
         }
 
 
-        if(resultVisible){
-            System.out.println();
-        }
+
 
     }
+
+    void drawReMenu(){
+        elevTre.tegnKnap();
+        elevTo.tegnKnap();
+        elevEn.tegnKnap();
+        nyElev.tegnKnap();
+        hold.tegnTextFlet();
+    }
+    void drawSPMenu(){
+        removeSpergsmaal.tegnTextFlet();
+        addSpergsmall.tegnTextFlet();
+        addRigtigtSvar.tegnTextFlet();
+        addSvar2.tegnTextFlet();
+        addSvar3.tegnTextFlet();
+        addSpergsmalK.tegnKnap();
+    }
+
     void tryk(char key){
-        removeSpergsmaal.keyindput(key);
-        addSpergsmall.keyindput(key);
-        addRigtigtSvar.keyindput(key);
-        addSvar2.keyindput(key);
-        addSvar3.keyindput(key);
+        if(editorVisible){
+            removeSpergsmaal.keyindput(key);
+            addSpergsmall.keyindput(key);
+            addRigtigtSvar.keyindput(key);
+            addSvar2.keyindput(key);
+            addSvar3.keyindput(key);
+        }
+
 
     }
     void clik(float mx, float my) {
-        addSpergsmalK.registrerKlik(p.mouseX,p.mouseY);
+        if(menuVisible){
+            editorKnap.registrerKlik(p.mouseX,p.mouseY);
+            System.out.println("mx: " + p.mouseX + " my: " + p.mouseY);
+            resultKnap.registrerKlik(mx,my);
+        }
 
+        if(editorVisible){
+            backToMenu.registrerKlik(mx,my);
+            addSpergsmalK.registrerKlik(p.mouseX,p.mouseY);
 
-        removeSpergsmaal.KlikTjek(mx, my);
-        addSpergsmall.KlikTjek(mx, my);
-        addRigtigtSvar.KlikTjek(mx, my);
-        addSvar2.KlikTjek(mx, my);
-        addSvar3.KlikTjek(mx, my);
+            if(editorKnap.klikket){
+                System.out.println("fack");
+            }
+
+            removeSpergsmaal.KlikTjek(mx, my);
+            addSpergsmall.KlikTjek(mx, my);
+            addRigtigtSvar.KlikTjek(mx, my);
+            addSvar2.KlikTjek(mx, my);
+            addSvar3.KlikTjek(mx, my);
+        }
+
+        if(resultVisible){
+            backToMenu.registrerKlik(mx,my);
+        }
+
 
     }
 
